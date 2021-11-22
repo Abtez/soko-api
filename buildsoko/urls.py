6c514d6b5418a948a30574a8cb20a205dfe1c589
Abzed Abdul Mohammed <ibraabzed@gmail.com>
2021-11-22 09:23:33 +0300

    add /

--- buildsoko/urls.py
+++ buildsoko/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('soko.urls')),
+    path('', include('soko.urls')),
     path('api/', include('soko.urls')),
 ]
--- soko/urls.py
+++ soko/urls.py
@@ -1,8 +1,9 @@
 from .views import AllProductsViews
 from django.urls import path
+from . import views
 
 urlpatterns=[
-    # path('', views.index, name='index'),
+    path('', views.index, name='index'),
     path('products/', AllProductsViews.as_view()),
     path('products/<slug:uuid>', AllProductsViews.as_view()),
     # path('products/<int:uuid>', views.product_detail)
